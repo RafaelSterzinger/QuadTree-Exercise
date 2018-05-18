ccd28daa1655feeddbb173613b1f417ebaa5d0fe
RafaelSterzinger <rafael.sterzinger@gmx.at>
2018-05-18 17:41:47 +0200

    Added Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try(Scanner s = new Scanner(
-                new File(System.getProperty("data") +
-                        "/data/junctions.csv"), "UTF-8")) {
+        ArrayList<String> data = new ArrayList<>();
+
+        try (Scanner s = new Scanner(
+                new File(System.getProperty("user.dir") +
+                        "/data/junctions.csv"), "UTF-8" )) {
             // Benutzen Sie das Scanner-Objekt s hier
-        } catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             // junctions.csv wurde nicht gefunden
             System.exit(1);
         }
-
     }
 }
