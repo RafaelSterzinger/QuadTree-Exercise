79b922ec8c1b7bd2cf60f15e15cc514b97ccc20f
RafaelSterzinger <rafael.sterzinger@gmx.at>
2018-05-18 17:01:16 +0200

    Test

--- src/NaiveSolution.java
+++ src/NaiveSolution.java
@@ -1,5 +1,6 @@
 public class NaiveSolution {
     public static void main(String[] args) {
-        System.out.println(5+6);
+        System.out.println(5+7);
+        System.out.println(11);
     }
 }
